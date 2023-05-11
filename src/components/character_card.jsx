3ca21f2e754f2bf666be723fad371d10ccd06c99
natacha aviles <natachaaviles@MacBook-Pro-de-natacha.local>
2023-05-11 12:16:36 -0400

    feat: add basic logic to game

--- src/components/character_card.jsx
+++ src/components/character_card.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
-function CharacterCard(props) {
-	const { name, species, status, image } = props.character
+const CharacterCard = React.forwardRef((props) => {
+	const { onClick, isFlipped, character } = props
+	const { name, species, status, image } = character
 
   return (
-    <div className='card'>
-			<img src={image} className='card__image' />
-			<p className='card__title'>{name}</p>
-			<p className='card__description'>{species} - {status}</p>
+    <div onClick={onClick} className={`card ${isFlipped ? 'card--flipped' : ''}`}>
+			<div className='card__front'>
+				<img src={image} className='card__image' />
+				<p className='card__title'>{name}</p>
+				<p className='card__description'>{species} - {status}</p>
+			</div>
+			<div className='card__back'>
+				<img src='src/assets/back_card.png' />
+			</div>
     </div>
   );
-}
+})
 
 export default CharacterCard;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,34 +1,69 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef, createRef } from 'react';
 
 import CharacterCard from '../components/character_card';
 import { replayGame } from '../services/api';
 
-function HomePage(props) {
-	const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
+function HomePage () {
+	const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
+	const [selected, setSelected] = useState([])
+	const [turn, setTurns] = useState(null)
 
 	useEffect(() => {
     async function fetchData() {
       try {
-        const newData = await replayGame();
-        setData(newData);
-        setLoading(false);
+        const newData = await replayGame()
+        setData(newData)
+        setLoading(false)
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error('Error fetching data:', error)
       }
     }
 
-    fetchData();
-  }, []);
-	console.log(data)
+    fetchData()
+  }, [])
+
+	const flipCard = (index, character) => {
+		setSelected(prevState => {
+      if (prevState.length > 1) {
+        return prevState;
+      } else {
+				const { name } = character
+        return [...prevState, {name, index}];
+      }
+    })
+
+		const foundObject = selected.some(item => item.name === character.name)
+
+		if (!foundObject && selected.length <= 1) {
+			setTimeout(() => {
+				setSelected([])
+			}, 1000);
+		} else if (foundObject) {
+			setTimeout(() => {
+				setData(prevState => prevState.filter(item => item.name !== character.name));
+			}, 1000);
+		}
+		
+	}
+
   return (
     <div className='container'>
 			<img src='src/assets/logo.png' />
+			<div className='game__title'>Juego de memoria</div>
       <div className='container-content'>
-			 {data?.map((character) => {
-				 return <CharacterCard character={character} />
+			 {data?.map((character, _index) => {
+				return (
+						<CharacterCard
+						key={_index} 
+						character={character} 
+						onClick={() => flipCard(_index, character)}
+						isFlipped={selected.some(item => item.index === _index)}
+					/>
+				 )
 			 })}
+			 <button className='game__button'>Jugar</button>
 	  	</div>
     </div>
   );
